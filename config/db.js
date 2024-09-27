b5eda1df16ed2f69fee300aee6b2f475f8095b4a
anujdubey88 <anujd1595@gmail.com>
2024-09-28 00:10:09 +0530

    added mongodbconnection

--- config/db.js
+++ config/db.js
@@ -2,7 +2,12 @@ const mongoose=require('mongoose');
 
 const connectDB=async()=>{
     try{
-        await mongoose.connect(process.env.MONGODB_URL);
+        await mongoose.connect(process.env.MONGODB_URL{
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            connectTimeoutMS: 20000,  // 20 seconds
+            socketTimeoutMS: 45000, 
+        });
         console.log(`db connected at: ${mongoose.connection.host}`);
     }
     catch(error){
