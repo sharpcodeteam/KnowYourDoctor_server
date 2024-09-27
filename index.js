d774b270e3d9a72a81120f3643d534099ee668ff
anujdubey88 <anujd1595@gmail.com>
2024-09-28 00:50:53 +0530

    cors origin

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(cookieParser());
 app.use(morgan('dev'));
 app.use(
   cors({
-    origin: 'https://knowyourdoctor.netlify.app',
+    origin: ['https://knowyourdoctor.netlify.app','http://localhost:3000'],
     credentials: true,
   })
 );
