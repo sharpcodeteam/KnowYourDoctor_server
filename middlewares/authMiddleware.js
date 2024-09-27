a3fbd6a7085dada1570b122fe149fe687a17584f
anujdubey88 <anujd1595@gmail.com>
2024-09-28 01:45:30 +0530

    authmiddleware

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,37 +1,37 @@
-const jwt = require("jsonwebtoken");
-require("dotenv").config();
+const JWT=require('jsonwebtoken');
 
-// auth------------------------------------------------
-exports.auth = async (req, res, next) => {
-  try {
-    const token =
-      req.cookies.login;
 
-    //if token missing, then return response
-    if (!token) {
-      return res.status(401).json({
-        success: false,
-        message: "Token is missing",
-      });
-    }
 
-    //verify the token
+module.exports=async(req,res,next)=>{
     try {
-      const decode = jwt.verify(token, process.env.JWT_SECRET);
-      req.user = decode;
-    } 
-    catch (err) {
-      //verification - issue
-      return res.status(401).json({
-        success: false,
-        message: "token is invalid",
-      });
-    }
-    next();
-  } catch (error) {
-    return res.status(401).json({
-      success: false,
-      message: "Something went wrong while validating the token",
-    });
-  }
-};
+        const token =
+          req.cookies.login;
+    
+        //if token missing, then return response
+        if (!token) {
+          return res.status(401).json({
+            success: false,
+            message: "Token is missing",
+          });
+        }
+    
+        //verify the token
+        try {
+          const decode = jwt.verify(token, process.env.JWT_SECRET);
+          req.user = decode;
+        } 
+        catch (err) {
+          //verification - issue
+          return res.status(401).json({
+            success: false,
+            message: "token is invalid",
+          });
+        }
+        next();
+      } catch (error) {
+        return res.status(401).json({
+          success: false,
+          message: "Something went wrong while validating the token",
+        });
+      }
+}
