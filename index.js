ae8e22ecebb25b4a98d9dc7b6d58e3de71eaf098
anujdubey88 <anujd1595@gmail.com>
2024-09-27 17:32:33 +0530

    index is changed

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(cookieParser());
 app.use(morgan('dev'));
 app.use(
   cors({
-    origin: ['http://localhost:3000'],
+    origin: ['https://knowyourdoctor.netlify.app'],
     credentials: true,
   })
 );
